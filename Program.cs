88e0f4c3670cc4cd7b49b6df5d1eb1be3c409754
Bogdan <hurasy71@gmail.com>
2023-01-09 19:00:24 +1000

    task1

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿System.Console.Write("Введите трехзначное число");
+
+int number = int.Parse(System.Console.ReadLine());
+int firstDigit = number / 10;
+int secondDigit = firstDigit % 10;
+
+ System.Console.WriteLine(secondDigit);
