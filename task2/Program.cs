cfbdda2055e2c6dedd2b4e153b01ec3dce6d66c5
Bogdan <hurasy71@gmail.com>
2023-01-09 21:47:56 +1000

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,20 @@
+﻿System.Console.Write("Введите число");
+
+int number = int.Parse(System.Console.ReadLine());
+
+if (number < 100)
+  {
+    System.Console.WriteLine("Третьей цифры нет");
+    
+  }
+  
+else
+{
+    System.Console.WriteLine("Третья цифра есть");
+}
+
+ int firstDigit = number / 10;
+ int secondDigit = firstDigit % 10;
+ int thirdDigit = secondDigit % 10; 
+
+System.Console.WriteLine(thirdDigit);
