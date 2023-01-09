d450c2388ef85ac89416ced8fe3e0d1e3c607603
Bogdan <hurasy71@gmail.com>
2023-01-09 22:39:07 +1000

    task3

--- task2/task3/Program.cs
+++ task2/task3/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.Write("Введите число от 1 до 7");
+
+int number = int.Parse(System.Console.ReadLine());
+
+if (number == 1)
+{
+    System.Console.WriteLine("не выходной");
+}
+
+if (number == 2)
+{
+    System.Console.WriteLine("не выходной");
+}
+
+if (number == 3)
+{
+    System.Console.WriteLine("не выходной");
+}
+
+if (number == 4)
+{
+    System.Console.WriteLine("не выходной");
+}
+
+if (number == 5)
+{
+    System.Console.WriteLine("не выходной");
+}
+
+if (number == 6)
+{
+    System.Console.WriteLine("выходной");
+}
+
+if (number == 7)
+{
+    System.Console.WriteLine("выходной");
+}
